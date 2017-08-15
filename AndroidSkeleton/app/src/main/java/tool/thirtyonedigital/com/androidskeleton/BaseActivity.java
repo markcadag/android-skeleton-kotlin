44e357703164afdb69dbb875631d7e627954eada
mark cadag <markcadag@gmail.com>
2017-08-15 16:26:56 +0800

    no message

---
+++ AndroidSkeleton/app/src/main/java/tool/thirtyonedigital/com/androidskeleton/BaseActivity.java
@@ -0,0 +1,9 @@
+package tool.thirtyonedigital.com.androidskeleton;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by markcadag on 09/08/2017.
+ */
+public class BaseActivity extends FragmentActivity {
+}
