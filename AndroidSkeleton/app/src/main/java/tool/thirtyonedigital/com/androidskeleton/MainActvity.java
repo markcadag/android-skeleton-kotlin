4e6d6f3d1533d368d1ab12855dfb3df18e80262d
mark cadag <markcadag@gmail.com>
2017-08-15 16:27:50 +0800

    Main Act

---
+++ AndroidSkeleton/app/src/main/java/tool/thirtyonedigital/com/androidskeleton/MainActvity.java
@@ -0,0 +1,11 @@
+package tool.thirtyonedigital.com.androidskeleton;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by markcadag on 09/08/2017.
+ */
+
+public class MainActvity extends BaseActivity {
+
+}
